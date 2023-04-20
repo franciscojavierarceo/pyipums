5a6a9b110eec2c69363b5de5a4b5ef6e4e9af673
Francisco Javier Arceo <franciscojavierarceo@users.noreply.github.com>
2023-04-20 01:50:25 -0400

    updated to include dummy test

---
+++ tests/test_parse_xml.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from src.pypums.parse_xml import read_ipums_ddi
+
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
+    def test_always_fails(self):
+        self.assertTrue(False)
