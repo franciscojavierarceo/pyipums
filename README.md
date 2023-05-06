cd960def3b7603f0d837e0f6c63b1aadf0fb0d05
Francisco Javier Arceo <franciscojavierarceo@users.noreply.github.com>
2023-05-05 20:59:01 -0400

    updated readme and adding a test

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# ipums-explore
+# PyPums
 
+PyPums is library for working with data from [IPUMS](https://www.ipums.org/).
+
+If you are looking to make changes to the library I recommend using [poetry](https://python-poetry.org/docs/).
 ```
 poetry env use 3.8
 pyenv shell 3.8
--- tests/test_parse_xml.py
+++ tests/test_parse_xml.py
@@ -1,9 +1,20 @@
 from unittest import TestCase
-from src.pypums.parse_xml import read_ipums_ddi
+from src.pypums.parse_xml import (
+    read_ipums_ddi,
+    _to_int,
+    remove_namespace,
+)
 
 class TryTesting(TestCase):
-    def test_always_passes(self):
-        self.assertTrue(True)
+    expected = ""
 
-    def test_always_fails(self):
-        self.assertTrue(False)
+    def test__to_int(self):
+        self.assertEqual(_to_int('1'), 1)
+
+    def test_remove_namespace(self):
+        self.assertEqual(
+            remove_namespace(
+                "{ddi:codebook:2_5}it works",
+            ),
+        "it works",
+        )
