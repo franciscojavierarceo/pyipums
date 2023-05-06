c8899071fb1297ec45a3d8c3f6f49ae731f09846
Francisco Javier Arceo <franciscojavierarceo@users.noreply.github.com>
2023-05-05 21:43:10 -0400

    updated unit tests

--- tests/test_parse_xml.py
+++ tests/test_parse_xml.py
@@ -1,13 +1,14 @@
 from unittest import TestCase
+import xml.etree.ElementTree as ET
 from src.pypums.parse_xml import (
     read_ipums_ddi,
     _to_int,
     remove_namespace,
+    get_file_metadata,
+    get_field_metadata,
 )
 
-class TryTesting(TestCase):
-    expected = ""
-
+class TestParseMetaDataXML(TestCase):
     def test__to_int(self):
         self.assertEqual(_to_int('1'), 1)
 
@@ -17,4 +18,47 @@ class TryTesting(TestCase):
                 "{ddi:codebook:2_5}it works",
             ),
         "it works",
+        )
+
+    def test_get_file_metadata(self):
+        metadata = {}
+        root = ET.parse("./metadata_example.xml")
+        codebook = root.getroot()
+        ddi_fields = [
+            'column_metadata',
+            'columns',
+            'column_types',
+            'column_specs',
+            'column_dtypes',
+        ]
+        metadata['file_metadata'] = get_file_metadata(codebook)
+
+        self.assertEqual(
+            len(metadata['file_metadata']),
+            10,
+        )
+        metadata = get_field_metadata(codebook, metadata)
+        for field in ddi_fields:
+            self.assertGreater(
+                len(metadata.get(field)),
+                0
+            )
+
+        self.assertEqual(
+            metadata.get('column_specs')[0],
+            (0, 4),
+        )
+
+        self.assertEqual(
+            metadata.get('column_metadata')[0],
+            ('YEAR', 'discrete'),
+        )
+
+        self.assertEqual(
+            metadata.get('column_dtypes')[0],
+            str,
+        )
+        self.assertEqual(
+            len(metadata.get('columns')),
+            149,
         )
