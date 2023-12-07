debb72e6adf0bf280d4775ead08318e16985e2d5
Francisco Javier Arceo <franciscojavierarceo@users.noreply.github.com>
2023-12-06 23:18:58 -0500

    linted the code and updated to format age

--- src/pyipums/clean_data.py
+++ src/pyipums/clean_data.py
@@ -3,29 +3,29 @@ import numpy as np
 from ipumspy import readers, ddi
 
 INCOME_COLUMN = "INC"
-EDUC_LT_HS = 'Less than High School Diploma'
-EDUC_HS = 'High school diploma or equivalent'
-EDUC_VOC = 'Vocational Degree'
+EDUC_LT_HS = "Less than High School Diploma"
+EDUC_HS = "High school diploma or equivalent"
+EDUC_VOC = "Vocational Degree"
 EDUC_BS = "Bachelor's degree"
 EDUC_GRAD = "Graduate degree"
 EDUC_ATTAINMENT = {
-    'NIU or blank': 'Missing',
-    'Grades 1, 2, 3, or 4': EDUC_LT_HS,
-    'Grades 5 or 6': EDUC_LT_HS,
-    'Grades 7 or 8': EDUC_LT_HS,
-    'Grade 9': EDUC_LT_HS,
-    'Grade 10': EDUC_LT_HS,
-    'Grade 11': EDUC_LT_HS,
-    '12th grade, no diploma': EDUC_LT_HS,
-    'None or preschool': EDUC_LT_HS,
-    'High school diploma or equivalent': EDUC_HS,
+    "NIU or blank": "Missing",
+    "Grades 1, 2, 3, or 4": EDUC_LT_HS,
+    "Grades 5 or 6": EDUC_LT_HS,
+    "Grades 7 or 8": EDUC_LT_HS,
+    "Grade 9": EDUC_LT_HS,
+    "Grade 10": EDUC_LT_HS,
+    "Grade 11": EDUC_LT_HS,
+    "12th grade, no diploma": EDUC_LT_HS,
+    "None or preschool": EDUC_LT_HS,
+    "High school diploma or equivalent": EDUC_HS,
     "Associate's degree, academic program": EDUC_HS,
     "Associate's degree, occupational/vocational program": EDUC_VOC,
-    'Some college but no degree': EDUC_HS,
+    "Some college but no degree": EDUC_HS,
     "Bachelor's degree": EDUC_BS,
     "Master's degree": EDUC_GRAD,
-    'Doctorate degree': EDUC_GRAD,
-    'Professional school degree': EDUC_GRAD,
+    "Doctorate degree": EDUC_GRAD,
+    "Professional school degree": EDUC_GRAD,
 }
 
 
@@ -49,49 +49,50 @@ class IpumsCleaner:
 
             self.df[f"{col}"] = self.df[col].replace(invalid_values, np.nan)
 
-        self.df['INCTOT'] = self.df['INCTOT'].astype(float)
+        self.df["INCTOT"] = self.df["INCTOT"].astype(float)
 
     def clean_educ_attainment(self):
-        self.df['Educational Attainment'] = self.df['Education'].apply(
-            lambda x: EDUC_ATTAINMENT.get(x)
-        ).astype(str)
+        self.df["Educational Attainment"] = (
+            self.df["Education"].apply(lambda x: EDUC_ATTAINMENT.get(x)).astype(str)
+        )
 
     def clean_variables(self):
-        self.df['Occupation'] = map_codes(self.ddi_codebook, self.df, 'OCC2010')
-        self.df['Education'] = map_codes(self.ddi_codebook, self.df, 'EDUC')
-        self.df['Birthplace'] = map_codes(self.ddi_codebook, self.df, 'BPL')
-        self.df['Marital_Status'] = map_codes(self.ddi_codebook, self.df, 'MARST')
-        self.df['Nativity'] = map_codes(self.ddi_codebook, self.df, 'NATIVITY')
-        self.df['Class_of_worker'] = map_codes(self.ddi_codebook, self.df, 'CLASSWKR')
-        self.df['Hispanic'] = map_codes(self.ddi_codebook, self.df, 'HISPAN')
-        self.df['Asian'] = map_codes(self.ddi_codebook, self.df, 'ASIAN')
-        self.df['Race'] = map_codes(self.ddi_codebook, self.df, 'RACE')
-        self.df['Veteran_Status'] = map_codes(self.ddi_codebook, self.df, 'VETSTAT')
+        self.df["Occupation"] = map_codes(self.ddi_codebook, self.df, "OCC2010")
+        self.df["Education"] = map_codes(self.ddi_codebook, self.df, "EDUC")
+        self.df["Birthplace"] = map_codes(self.ddi_codebook, self.df, "BPL")
+        self.df["Marital_Status"] = map_codes(self.ddi_codebook, self.df, "MARST")
+        self.df["Nativity"] = map_codes(self.ddi_codebook, self.df, "NATIVITY")
+        self.df["Class_of_worker"] = map_codes(self.ddi_codebook, self.df, "CLASSWKR")
+        self.df["Hispanic"] = map_codes(self.ddi_codebook, self.df, "HISPAN")
+        self.df["Asian"] = map_codes(self.ddi_codebook, self.df, "ASIAN")
+        self.df["Race"] = map_codes(self.ddi_codebook, self.df, "RACE")
+        self.df["Veteran_Status"] = map_codes(self.ddi_codebook, self.df, "VETSTAT")
+        self.df["Age"] = self.df["AGE"].astype(float)
 
     def clean_wages(self):
         # Aggregating Wages
         income_buckets = {
-            'INCSS': 'Government',
-            'INCWELFR': 'Government',
-            'INCRETIR': 'Investment',
-            'INCSSI': 'Government',
-            'INCINT': 'Investment',
-            'INCUNEMP': 'Government',
-            'INCWKCOM': 'Wage',
-            'INCVET': 'Government',
-            'INCSURV': 'Government',
-            'INCDISAB': 'Government',
-            'INCDIVID': 'Investment',
-            'INCRENT': 'Investment',
-            'INCEDUC': 'Government',
-            'INCCHILD': 'Government',
-            'INCASIST': 'Government',
-            'INCOTHER': 'Unknown',
-            'INCRANN': 'Investment',
-            'INCPENS': 'Wage',
-            'INCWAGE': 'Wage',
-            'INCBUS': 'Wage',
-            'INCFARM': 'Wage',
+            "INCSS": "Government",
+            "INCWELFR": "Government",
+            "INCRETIR": "Investment",
+            "INCSSI": "Government",
+            "INCINT": "Investment",
+            "INCUNEMP": "Government",
+            "INCWKCOM": "Wage",
+            "INCVET": "Government",
+            "INCSURV": "Government",
+            "INCDISAB": "Government",
+            "INCDIVID": "Investment",
+            "INCRENT": "Investment",
+            "INCEDUC": "Government",
+            "INCCHILD": "Government",
+            "INCASIST": "Government",
+            "INCOTHER": "Unknown",
+            "INCRANN": "Investment",
+            "INCPENS": "Wage",
+            "INCWAGE": "Wage",
+            "INCBUS": "Wage",
+            "INCFARM": "Wage",
         }
 
         column_to_bucket_map = {}
@@ -106,36 +107,69 @@ class IpumsCleaner:
         for k in column_to_bucket_map:
             self.df[f"{k} Income"] = self.df[column_to_bucket_map[k]].sum(axis=1)
 
-        self.df['Investment Income as Percent of Total Income'] = np.where(
-            self.df['INCTOT'] == 0,
+        self.df["Investment Income as Percent of Total Income"] = np.where(
+            self.df["INCTOT"] == 0,
             0,
-            self.df['Investment Income'].astype(float) / self.df['INCTOT'].astype(float))
+            self.df["Investment Income"].astype(float)
+            / self.df["INCTOT"].astype(float),
+        )
 
-        self.df['Government Income as Percent of Total Income'] = np.where(
-            self.df['INCTOT'] == 0,
+        self.df["Government Income as Percent of Total Income"] = np.where(
+            self.df["INCTOT"] == 0,
             0,
-            self.df['Government Income'].astype(float) / self.df['INCTOT'].astype(float))
+            self.df["Government Income"].astype(float)
+            / self.df["INCTOT"].astype(float),
+        )
 
-        self.df['Wage Income as Percent of Total Income'] = np.where(
-            self.df['INCTOT'] == 0,
+        self.df["Wage Income as Percent of Total Income"] = np.where(
+            self.df["INCTOT"] == 0,
             0,
-            self.df['Wage Income'].astype(float) / self.df['INCTOT'].astype(float))
+            self.df["Wage Income"].astype(float) / self.df["INCTOT"].astype(float),
+        )
 
-        self.df.loc[self.df['Investment Income as Percent of Total Income'] < 0, 'Investment Income as Percent of Total Income'] = 0.
-        self.df.loc[self.df['Investment Income as Percent of Total Income'] > 1, 'Investment Income as Percent of Total Income'] = 1
-        self.df.loc[self.df['Government Income as Percent of Total Income'] < 0, 'Government Income as Percent of Total Income'] = 0.
-        self.df.loc[self.df['Government Income as Percent of Total Income'] > 1, 'Government Income as Percent of Total Income'] = 1
-        self.df.loc[self.df['Wage Income as Percent of Total Income'] < 0, 'Wage Income as Percent of Total Income'] = 0.
-        self.df.loc[self.df['Wage Income as Percent of Total Income'] > 1, 'Wage Income as Percent of Total Income'] = 1
+        self.df.loc[
+            self.df["Investment Income as Percent of Total Income"] < 0,
+            "Investment Income as Percent of Total Income",
+        ] = 0.0
+        self.df.loc[
+            self.df["Investment Income as Percent of Total Income"] > 1,
+            "Investment Income as Percent of Total Income",
+        ] = 1
+        self.df.loc[
+            self.df["Government Income as Percent of Total Income"] < 0,
+            "Government Income as Percent of Total Income",
+        ] = 0.0
+        self.df.loc[
+            self.df["Government Income as Percent of Total Income"] > 1,
+            "Government Income as Percent of Total Income",
+        ] = 1
+        self.df.loc[
+            self.df["Wage Income as Percent of Total Income"] < 0,
+            "Wage Income as Percent of Total Income",
+        ] = 0.0
+        self.df.loc[
+            self.df["Wage Income as Percent of Total Income"] > 1,
+            "Wage Income as Percent of Total Income",
+        ] = 1
 
-        self.df['Weighted Total Income'] = self.df['INCTOT'] * self.df['ASECWT']
-        self.df['Weighted Government Income'] = self.df['Government Income'] * self.df['ASECWT']
-        self.df['Weighted Investment Income'] = self.df['Investment Income'] * self.df['ASECWT']
-        self.df['Weighted Wage Income'] = self.df['Wage Income'] * self.df['ASECWT']
+        self.df["Weighted Total Income"] = self.df["INCTOT"] * self.df["ASECWT"]
+        self.df["Weighted Government Income"] = (
+            self.df["Government Income"] * self.df["ASECWT"]
+        )
+        self.df["Weighted Investment Income"] = (
+            self.df["Investment Income"] * self.df["ASECWT"]
+        )
+        self.df["Weighted Wage Income"] = self.df["Wage Income"] * self.df["ASECWT"]
 
-        self.df['Weighted Government Income as Percent of Total Income'] = self.df['Government Income as Percent of Total Income'] * self.df['ASECWT']
-        self.df['Weighted Investment Income as Percent of Total Income'] = self.df['Investment Income as Percent of Total Income'] * self.df['ASECWT']
-        self.df['Weighted Wage Income as Percent of Total Income'] = self.df['Wage Income as Percent of Total Income'] * self.df['ASECWT']
+        self.df["Weighted Government Income as Percent of Total Income"] = (
+            self.df["Government Income as Percent of Total Income"] * self.df["ASECWT"]
+        )
+        self.df["Weighted Investment Income as Percent of Total Income"] = (
+            self.df["Investment Income as Percent of Total Income"] * self.df["ASECWT"]
+        )
+        self.df["Weighted Wage Income as Percent of Total Income"] = (
+            self.df["Wage Income as Percent of Total Income"] * self.df["ASECWT"]
+        )
 
     def clean_data(self):
         self.clean_variables()
